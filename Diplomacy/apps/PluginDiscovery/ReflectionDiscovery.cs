e8417e4195e750914f82f441ff462607b4f7221d
Aaron <aaron.schnarr@codeconsulate.com>
2017-05-01 13:06:21 -0700

    Added project to demonstrate Reflection and MEF discovery functionality.

---
+++ Diplomacy/apps/PluginDiscovery/ReflectionDiscovery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginDiscovery
+{
+    /// <summary>
+    /// Class used to demonstrate "plugin discovery" using legacy .NET Reflection.
+    /// </summary>
+    public class ReflectionDiscovery
+    {
+
+    }
+}
