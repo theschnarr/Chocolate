2dd1eebdb792f9d929f054cc0f9d003ad0f5234c
Aaron <aaron.schnarr@codeconsulate.com>
2017-05-05 22:39:33 -0700

    Moved Plugins Folder up to root directory, as Plugins are no longer used for testing other classes, which is instead using the dynamic plugin classes compiled at runtime.

---
+++ Diplomacy/Plugins/DemoPluginTests/WordCountAmbassadorTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextPlugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Diplomacy.Kit;
+
+namespace TextPlugins.Tests
+{
+    [TestClass()]
+    public class WordCountAmbassadorTests
+    {
+        [TestMethod()]
+        public void ProcessTest()
+        {
+            var wAmbassador = new WordCountAmbassador();
+            var result = wAmbassador.Process("Some JSON string here");
+            Assert.AreEqual(result.Code, ResultCode.Success);
+        }
+        [TestMethod]
+        public void ProcessNullTest()
+        {
+            var wAmbassador = new WordCountAmbassador();
+            var result = wAmbassador.Process(null);
+            Assert.AreEqual(result.Code, ResultCode.InvalidArgument);
+        }
+        [TestMethod()]
+        public void StartTest()
+        {
+            var wAmbassador = new WordCountAmbassador();
+            var result = wAmbassador.Start();
+            Assert.AreEqual(result.Code, ResultCode.Success);
+        }
+
+        [TestMethod()]
+        public void StopTest()
+        {
+            var wAmbassador = new WordCountAmbassador();
+            var result = wAmbassador.Stop();
+            Assert.AreEqual(result.Code, ResultCode.Success);
+        }
+        [TestMethod()]
+        public void IDTest()
+        {
+            var wAmbassador = new WordCountAmbassador();
+            Assert.AreEqual(wAmbassador.ID, "WordCountAmbassador");
+        }
+        [TestMethod()]
+        public void SupportedActionsTest()
+        {
+            var wAmbassador = new WordCountAmbassador();
+            CollectionAssert.AreEquivalent(wAmbassador.SupportedActions.ToList(), new List<string>() { "WordCount" });
+        }
+    }
+}
